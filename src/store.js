abe7e29a4bb3f385eb41a672d665356efeaada03
pivarci <pivarci.matej@gmail.com>
2017-03-28 00:21:38 +0200

    redux logger commented out

--- src/store.js
+++ src/store.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import ReduxPromise from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+// import createLogger from 'redux-logger';
 
 import reducers from './reducers';
 
-const logger = createLogger();
-const middleware = applyMiddleware(ReduxThunk, ReduxPromise, logger);
+// const logger = createLogger();
+// const middleware = applyMiddleware(ReduxThunk, ReduxPromise, logger);
+const middleware = applyMiddleware(ReduxThunk, ReduxPromise);
 
 export default createStore(reducers, compose(middleware));
